ce06d0572526aa4045ca93515289fad8b603740e
Carlos Perez Lara <dam24-27@vedrunacentroseducativos.onmicrosoft.com>
2025-06-04 19:29:41 +0200

    Añadidos Main y clase Ahorcado con método iniciar, incluyendo modificaciones

---
+++ src/main/java/Ahorcado/Ahorcado.java
@@ -0,0 +1,9 @@
+package Ahorcado;
+
+public class Ahorcado {
+        public void iniciar() {
+            System.out.println("¡El juego ha empezado!");
+            // resto de código...
+        }
+    }
+
--- src/main/java/Ahorcado/Main.java
+++ src/main/java/Ahorcado/Main.java
@@ -1,4 +1,7 @@
 package Ahorcado;
-
-import java.util.Scanner;
-import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Ahorcado juego = new Ahorcado();
+        juego.iniciar();
+    }
+}
